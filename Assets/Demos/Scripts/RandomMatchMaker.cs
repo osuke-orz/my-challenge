65bf8858bbe0e44531c39fac779c5483334e6637
osuke-orz <osuke20080310@gmail.com>
2025-10-21 13:03:29 +0900

    ok

--- Assets/Demos/Scripts/RandomMatchMaker.cs
+++ Assets/Demos/Scripts/RandomMatchMaker.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.TextCore.Text;
 
 public class RandomMatchMaker : MonoBehaviourPunCallbacks
 {
     //インスペクターから設定
     public GameObject PhotonObject;
+    public GameObject weapon;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,12 +37,30 @@ public class RandomMatchMaker : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.Instantiate(
+        GameObject Photonplayer= PhotonNetwork.Instantiate(
             PhotonObject.name,
             new Vector3(0f, 1f, 0f),
             Quaternion.identity,
             0
             );
+
+        GameObject weaponInstance = PhotonNetwork.Instantiate(
+            weapon.name,
+            new Vector3(0f, 1f, 0f),
+            Quaternion.identity,
+            0
+        );
+
+
+
+
+        Transform handTransform = Photonplayer.transform.Find("Character1_Reference/Character1_Hips/Character1_Spine/Character1_Spine1/Character1_Spine2/Character1_RightShoulder/Character1_RightArm/Character1_RightForeArm/Character1_RightHand"); // モデルに合わせてパスを調整
+        weaponInstance.transform.SetParent(handTransform);
+        weaponInstance.transform.localPosition = Vector3.zero; // 手の位置に合わせる
+        weaponInstance.transform.localRotation = Quaternion.identity; // 回転も合わせる
+
+
+
 
         GameObject mainCamera = GameObject.FindWithTag("MainCamera");
         mainCamera.GetComponent<newTPS>().enabled = true;
---
+++ Assets/first gun model/hand.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class hand : MonoBehaviour
+{
+    private Transform target; // キャラクターのTransform
+    public Vector3 offset = new Vector3(0, 0, 0); // 頭上に表示
+
+    void Start()
+    {
+        StartCoroutine(WaitAndSetTarget());
+    }
+
+    IEnumerator WaitAndSetTarget()
+    {
+        while (target == null)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject player in players)
+            {
+                PhotonView pv = player.GetComponent<PhotonView>();
+                if (pv != null && pv.IsMine)
+                {
+                    target = player.transform;
+                    Debug.Log("target を設定: " + target.name);
+                    yield break;
+                }
+            }
+            yield return null; // 次のフレームまで待つ
+        }
+    }
+    void LateUpdate()
+    {
+        {
+            if (target == null)
+            {
+                Debug.LogWarning("target が設定されていません！");
+                return;
+            }
+
+            Debug.Log(transform.name + "の位置: " + transform.position);
+
+            transform.position = target.position + offset;
+        }
+    }
+}
